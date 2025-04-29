e5d71b2daed6e8409f3c939f570431b822578892
Nikhil Tirunagiri <71447707+NikhilTirunagiri@users.noreply.github.com>
2025-04-29 11:35:26 -0400

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/nt/Desktop/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/nt/Desktop/catkin_ws/devel;/home/nt/Desktop/CleaningRobot/devel;/opt/ros/noetic'.split(';') if '/home/nt/Desktop/catkin_ws/devel;/home/nt/Desktop/CleaningRobot/devel;/opt/ros/noetic' != '' else []
